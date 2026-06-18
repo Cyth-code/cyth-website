1fe40c7d64deb26b2ba5603c5c08c5790256ff0b
CythDom <dominic.spinozzi@cyth.com>
2026-06-18 10:26:48 -0700

    Fixed Mario's Event Logic
    Contact and Engineer field in CMS now shows correct form on page
    Reserve your Spot field in CMS now shows correct form on page

--- src/pages/Events (Item).jtkf7.js
+++ src/pages/Events (Item).jtkf7.js
@@ -1,24 +1,33 @@
 // Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 
+import { L, E } from 'backend/logger.web';
+
 $w.onReady(function () {
 	$w("#dynamicDataset").onReady(() => {
-		const currentEvent = $w("#dynamicDataset").getCurrentItem()
-		const rsvpSection = $w("#rsvpSection");
-		const rsvpButton = $w("#rsvpButton");
+		
 
-		if(currentEvent?.show_rsvp){
-			rsvpSection.expand?.()
-			rsvpButton.expand?.()
+		try
+		{
+			
+			const currentEvent = $w("#dynamicDataset").getCurrentItem()
+			const engineerSection = $w("#rsvpSection");
+			const reserveSection = $w("#section71");
+
+			if(currentEvent?.engineer_form){ //field ID for "Connect with and Engineer" 
+				engineerSection.expand?.()
+			}
+
+			if(currentEvent?.show_rsvp){
+				reserveSection.expand?.();
+			}
+
+			const formValues = {
+				"event_interested_in_1": currentEvent.title
+			}
+			$w("#form1").setFieldValues(formValues);
+
 		}
-
-		rsvpButton.onClick(() => {
-			rsvpSection.scrollTo?.();
-		});
-
-		const formValues = {
-			"event_interested_in_1": currentEvent.title
-		}
-		$w("#form1").setFieldValues(formValues)
+		catch(err){E("Events (Item)", err)}
 	})
 
 });
