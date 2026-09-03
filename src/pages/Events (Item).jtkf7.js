59b2f534f830fa50f08c1e9e75957ca2c678fba9
Cyth-code <noopura.vaidya@cyth.com>
2026-09-03 16:02:07 -0700

    Refactor event handling and form value setting

--- src/pages/Events (Item).jtkf7.js
+++ src/pages/Events (Item).jtkf7.js
@@ -1,33 +1,32 @@
-// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-
-import { L, E } from 'backend/logger.web';
-
 $w.onReady(function () {
-	$w("#dynamicDataset").onReady(() => {
-		
+  $w("#dynamicDataset").onReady(() => {
 
-		try
-		{
-			
-			const currentEvent = $w("#dynamicDataset").getCurrentItem()
-			const engineerSection = $w("#rsvpSection");
-			const reserveSection = $w("#section71");
+    try {
+      const currentEvent = $w("#dynamicDataset").getCurrentItem();
+      const engineerSection = $w("#rsvpSection");
+      const reserveSection = $w("#section71");
 
-			if(currentEvent?.engineer_form){ //field ID for "Connect with and Engineer" 
-				engineerSection.expand?.()
-			}
+      if (currentEvent?.engineer_form) {
+        engineerSection.expand?.()
+      }
 
-			if(currentEvent?.show_rsvp){
-				reserveSection.expand?.();
-			}
+      if (currentEvent?.show_rsvp) {
+        reserveSection.expand?.();
+      }
 
-			const formValues = {
-				"event_interested_in_1": currentEvent.title
-			}
-			$w("#form1").setFieldValues(formValues);
+      const formValues = {
+        "event_interested_in_1": currentEvent.title
+      }
+      $w("#form1")?.setFieldValues?.(formValues);
 
-		}
-		catch(err){E("Events (Item)", err)}
-	})
+      const saveMySpotValues = {
+        "event_name": currentEvent.title || '',
+        "event_date": currentEvent.date ? new Date(currentEvent.date).toDateString() : '',
+        "event_location": currentEvent.location || '',
+        "booth_number": currentEvent.boothNumber || ''
+      }
+      $w("#form2")?.setFieldValues?.(saveMySpotValues);
 
+    } catch (err) { E("Events (Item)", err) }
+  })
 });
